a127f0e396555204bf2ab19bdea262c64e5e0e08
zjh <1710247022@qq.com>
2023-03-14 16:46:13 +0800

    添加公共模块

---
+++ fsapi_common/src/main/java/com/xxfs/fsapicommon/service/InnerUserInterfaceInfoService.java
@@ -0,0 +1,16 @@
+package com.xxfs.fsapicommon.service;
+
+/**
+ *
+ */
+public interface InnerUserInterfaceInfoService {
+
+    /**
+     * 调用接口统计
+     *
+     * @param interfaceInfoId
+     * @param userId
+     * @return
+     */
+    boolean invokeCount(long interfaceInfoId, long userId);
+}
