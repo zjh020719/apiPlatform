aa76bcf3d323cdb9b608384f43a9354c831ec56d
zjh <1710247022@qq.com>
2023-04-03 23:25:16 +0800

    优化爬虫接口,完善爬虫接口返回逻辑

--- fsapi_school_crawler/src/main/java/com/xxfs/fsapischoolcrawler/manager/CookiesManager.java
+++ fsapi_school_crawler/src/main/java/com/xxfs/fsapischoolcrawler/manager/CookiesManager.java
@@ -1,30 +1,27 @@
 package com.xxfs.fsapischoolcrawler.manager;
 
-import org.springframework.stereotype.Component;
-
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-
+import org.springframework.stereotype.Component;
 
 /**
  * @author zjh
  */
 @Component
 public class CookiesManager {
-    private final Map<String, String> cookies;
+  private final Map<String, String> cookies;
 
-    public CookiesManager() {
-        this.cookies = new HashMap<>();
-    }
+  public CookiesManager() {
+    this.cookies = new HashMap<>();
+  }
 
-    public void setCookies(Map<String, String> cookies) {
-        this.cookies.clear();
-        this.cookies.putAll(cookies);
-    }
+  public void setCookies(Map<String, String> cookies) {
+    this.cookies.clear();
+    this.cookies.putAll(cookies);
+  }
 
-    public Map<String, String> getCookies() {
-        return Collections.unmodifiableMap(this.cookies);
-    }
+  public Map<String, String> getCookies() {
+    return Collections.unmodifiableMap(this.cookies);
+  }
 }
-
