c9b3b3d49605d291a64f6983534b000af7e233f1
zjh <1710247022@qq.com>
2023-04-03 23:27:53 +0800

    优化爬虫接口,完善爬虫接口返回逻辑

--- fsapi_gateway/src/main/java/com/xxfs/fsapigateway/CustomGlobalFilter.java
+++ fsapi_gateway/src/main/java/com/xxfs/fsapigateway/CustomGlobalFilter.java
@@ -1,6 +1,5 @@
 package com.xxfs.fsapigateway;
 
-
 import com.auth0.jwt.exceptions.AlgorithmMismatchException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
@@ -43,221 +42,220 @@ import java.util.List;
 @Slf4j
 @Component
 public class CustomGlobalFilter implements GlobalFilter, Ordered {
-    private final TokenBucket tokenBucket; // 容量为 100，每秒产生 10 个令牌
-    @DubboReference
-    private InnerUserService innerUserService;
+  private static final List<String> IP_WHITE_LIST = Arrays.asList("127.0.0.1");
+  private static final String INTERFACE_HOST = "http://localhost:8123";
+  private final TokenBucket tokenBucket; // 容量为 50，每秒产生 5 个令牌
+  @DubboReference
+  private InnerUserService innerUserService;
+  @DubboReference
+  private InnerInterfaceInfoService innerInterfaceInfoService;
+  @DubboReference
+  private InnerUserInterfaceInfoService innerUserInterfaceInfoService;
 
-    @DubboReference
-    private InnerInterfaceInfoService innerInterfaceInfoService;
+  public CustomGlobalFilter() {
+    this.tokenBucket = new TokenBucket(10, 2);
+  }
 
-    @DubboReference
-    private InnerUserInterfaceInfoService innerUserInterfaceInfoService;
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    // 获取token
+    ServerHttpResponse response = exchange.getResponse();
+    ServerHttpRequest request = exchange.getRequest();
+    HttpHeaders headers = request.getHeaders();
+    String accessKey = headers.getFirst("accessKey");
+    String requestPath = request.getPath().value();
 
-    private static final List<String> IP_WHITE_LIST = Arrays.asList("127.0.0.1");
+    //  实际情况应该是去数据库中查是否已分配给用户
+    User invokeUser = null;
+    if (accessKey != null) {
+      try {
+        invokeUser = innerUserService.getInvokeUser(accessKey);
+      } catch (Exception e) {
+        log.error("getInvokeUser error", e);
+      }
+    }
+    if (requestPath.matches("^/api/interface.*")) {
+      if (!tokenBucket.tryAcquire(1)) {
+        return handleManyRequest(response);
+      }
+      // 1. 请求日志
+      requestPath = requestPath.replaceFirst("/api/interface", "");
+      String method = request.getMethod().toString().toLowerCase();
+      log.info("请求唯一标识：" + request.getId());
+      log.info("请求路径：" + requestPath);
+      log.info("请求方法：" + method);
+      log.info("请求参数：" + request.getQueryParams());
+      String sourceAddress = request.getLocalAddress().getHostString();
+      log.info("请求来源地址：" + sourceAddress);
+      log.info("请求来源地址：" + request.getRemoteAddress());
 
-    private static final String INTERFACE_HOST = "http://localhost:8123";
+      // 2. 访问控制 - 黑白名单
+      if (!IP_WHITE_LIST.contains(sourceAddress)) {
+        response.setStatusCode(HttpStatus.FORBIDDEN);
+        return response.setComplete();
+      }
+      // 3. 用户鉴权（判断 ak、sk 是否合法）
+      String nonce = headers.getFirst("nonce");
+      String timestamp = headers.getFirst("timestamp");
+      String sign = headers.getFirst("sign");
+      String body = headers.getFirst("body");
 
-    public CustomGlobalFilter() {
-        this.tokenBucket = new TokenBucket(50, 5);
+      if (invokeUser == null) {
+        return handleNoAuth(response);
+      }
+      //        if (!"yupi".equals(accessKey)) {
+      //            return handleNoAuth(response);
+      //        }
+      if (Long.parseLong(nonce) > 10000L || Long.parseLong(nonce) <= 0) {
+        return handleNoAuth(response);
+      }
+      // 时间和当前时间不能超过 5 分钟
+      Long currentTime = System.currentTimeMillis() / 1000;
+      final Long FIVE_MINUTES = 60 * 5L;
+      log.info("timestamp:{}", timestamp);
+      if ((currentTime - Long.parseLong(timestamp)) >= FIVE_MINUTES) {
+        return handleNoAuth(response);
+      }
+      // 实际情况中是从数据库中查出 secretKey
+      String secretKey = invokeUser.getSecretKey();
+      String serverSign = SignUtils.genSign(body, secretKey);
+      if (sign == null || !sign.equals(serverSign)) {
+        return handleNoAuth(response);
+      }
+      // 4. 请求的模拟接口是否存在，以及请求方法是否匹配
+      InterfaceInfo interfaceInfo = null;
+      try {
+        interfaceInfo = innerInterfaceInfoService.getInterfaceInfo(requestPath, method);
+      } catch (Exception e) {
+        log.error("getInterfaceInfo error", e);
+      }
+      if (interfaceInfo == null) {
+        return handleNoAuth(response);
+      }
+      //  是否还有调用次数
+      if (!innerUserInterfaceInfoService.invokeLeftNum(interfaceInfo.getId(), invokeUser.getId())) {
+        return handleNoAuth(response);
+      }
+      // 5. 请求转发，调用模拟接口 + 响应日志
+      //        Mono<Void> filter = chain.filter(exchange);
+      //        return filter;
+      return handleResponse(exchange, chain, interfaceInfo.getId(), invokeUser.getId());
     }
 
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        //获取token
-        ServerHttpResponse response = exchange.getResponse();
-        ServerHttpRequest request = exchange.getRequest();
-        HttpHeaders headers = request.getHeaders();
-        String accessKey = headers.getFirst("accessKey");
-        String requestPath = request.getPath().value();
-
-        //  实际情况应该是去数据库中查是否已分配给用户
-        User invokeUser = null;
-        if (accessKey != null) {
-            try {
-                invokeUser = innerUserService.getInvokeUser(accessKey);
-            } catch (Exception e) {
-                log.error("getInvokeUser error", e);
-            }
-        }
-        if (requestPath.matches("^/api/interface.*")) {
-            if (!tokenBucket.tryAcquire(1)) {
-                return handleManyRequest(response);
-            }
-            // 1. 请求日志
-            requestPath = requestPath.replaceFirst("/api/interface", "");
-            String method = request.getMethod().toString().toLowerCase();
-            log.info("请求唯一标识：" + request.getId());
-            log.info("请求路径：" + requestPath);
-            log.info("请求方法：" + method);
-            log.info("请求参数：" + request.getQueryParams());
-            String sourceAddress = request.getLocalAddress().getHostString();
-            log.info("请求来源地址：" + sourceAddress);
-            log.info("请求来源地址：" + request.getRemoteAddress());
-
-            // 2. 访问控制 - 黑白名单
-            if (!IP_WHITE_LIST.contains(sourceAddress)) {
-                response.setStatusCode(HttpStatus.FORBIDDEN);
-                return response.setComplete();
-            }
-            // 3. 用户鉴权（判断 ak、sk 是否合法）
-            String nonce = headers.getFirst("nonce");
-            String timestamp = headers.getFirst("timestamp");
-            String sign = headers.getFirst("sign");
-            String body = headers.getFirst("body");
-
-            if (invokeUser == null) {
-                return handleNoAuth(response);
-            }
-//        if (!"yupi".equals(accessKey)) {
-//            return handleNoAuth(response);
-//        }
-            if (Long.parseLong(nonce) > 10000L || Long.parseLong(nonce) <= 0) {
-                return handleNoAuth(response);
-            }
-            // 时间和当前时间不能超过 5 分钟
-            Long currentTime = System.currentTimeMillis() / 1000;
-            final Long FIVE_MINUTES = 60 * 5L;
-            log.info("timestamp:{}", timestamp);
-            if ((currentTime - Long.parseLong(timestamp)) >= FIVE_MINUTES) {
-                return handleNoAuth(response);
-            }
-            // 实际情况中是从数据库中查出 secretKey
-            String secretKey = invokeUser.getSecretKey();
-            String serverSign = SignUtils.genSign(body, secretKey);
-            if (sign == null || !sign.equals(serverSign)) {
-                return handleNoAuth(response);
-            }
-            // 4. 请求的模拟接口是否存在，以及请求方法是否匹配
-            InterfaceInfo interfaceInfo = null;
-            try {
-                interfaceInfo = innerInterfaceInfoService.getInterfaceInfo(requestPath, method);
-            } catch (Exception e) {
-                log.error("getInterfaceInfo error", e);
-            }
-            if (interfaceInfo == null) {
-                return handleNoAuth(response);
-            }
-            // todo 是否还有调用次数
-            if (!innerUserInterfaceInfoService.invokeLeftNum(interfaceInfo.getId(), invokeUser.getId())) {
-                return handleNoAuth(response);
-            }
-            // 5. 请求转发，调用模拟接口 + 响应日志
-            //        Mono<Void> filter = chain.filter(exchange);
-            //        return filter;
-            return handleResponse(exchange, chain, interfaceInfo.getId(), invokeUser.getId());
-        }
-
-
-        if (!"/api/backend/user/login".equals(requestPath) && !"/api/backend/user/register".equals(requestPath) && !"/api/backend/user/logout".equals(requestPath)) {
-            String token = exchange.getRequest().getHeaders().getFirst("Authentication");
-            if (StringUtils.isBlank(token)) {
-                log.error("token为空");
-                //没有token
-                return handleNoAuth(response);
-            } else {
-                //有token
-                try {
-                    JWTUtils.verify(token, invokeUser.getSecretKey());
-                } catch (SignatureVerificationException e) {
-                    log.error("无效签名！ 错误 ->", e);
-                    return handleNoAuth(response);
-                } catch (TokenExpiredException e) {
-                    log.error("token过期！ 错误 ->", e);
-                    return handleNoAuth(response);
-                } catch (AlgorithmMismatchException e) {
-                    log.error("token算法不一致！ 错误 ->", e);
-                    return handleNoAuth(response);
-                } catch (Exception e) {
-                    log.error(e.getMessage(), e);
-                    return handleNoAuth(response);
-                }
-            }
-        }
-
-
-        return chain.filter(exchange);
-    }
-
-    /**
-     * 处理响应
-     *
-     * @param exchange
-     * @param chain
-     * @return
-     */
-    public Mono<Void> handleResponse(ServerWebExchange exchange, GatewayFilterChain chain, long interfaceInfoId, long userId) {
+    if (!"/api/backend/user/login".equals(requestPath)
+            && !"/api/backend/user/register".equals(requestPath)
+            && !"/api/backend/user/logout".equals(requestPath)) {
+      String token = exchange.getRequest().getHeaders().getFirst("Authentication");
+      if (StringUtils.isBlank(token)) {
+        log.error("token为空");
+        // 没有token
+        return handleNoAuth(response);
+      } else {
+        // 有token
         try {
-            ServerHttpResponse originalResponse = exchange.getResponse();
-            // 缓存数据的工厂
-            DataBufferFactory bufferFactory = originalResponse.bufferFactory();
-            // 拿到响应码
-            HttpStatus statusCode = originalResponse.getStatusCode();
-            if (statusCode == HttpStatus.OK) {
-                // 装饰，增强能力
-                ServerHttpResponseDecorator decoratedResponse = new ServerHttpResponseDecorator(originalResponse) {
-                    // 等调用完转发的接口后才会执行
-                    @Override
-                    public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
-                        log.info("body instanceof Flux: {}", (body instanceof Flux));
-                        if (body instanceof Flux) {
-                            Flux<? extends DataBuffer> fluxBody = Flux.from(body);
-                            // 往返回值里写数据
-                            // 拼接字符串
-                            return super.writeWith(
-                                    fluxBody.map(dataBuffer -> {
+          JWTUtils.verify(token, invokeUser.getSecretKey());
+        } catch (SignatureVerificationException e) {
+          log.error("无效签名！ 错误 ->", e);
+          return handleNoAuth(response);
+        } catch (TokenExpiredException e) {
+          log.error("token过期！ 错误 ->", e);
+          return handleNoAuth(response);
+        } catch (AlgorithmMismatchException e) {
+          log.error("token算法不一致！ 错误 ->", e);
+          return handleNoAuth(response);
+        } catch (Exception e) {
+          log.error(e.getMessage(), e);
+          return handleNoAuth(response);
+        }
+      }
+    }
+
+    return chain.filter(exchange);
+  }
+
+  /**
+   * 处理响应
+   *
+   * @param exchange
+   * @param chain
+   * @return
+   */
+  public Mono<Void> handleResponse(
+          ServerWebExchange exchange, GatewayFilterChain chain, long interfaceInfoId, long userId) {
+    try {
+      ServerHttpResponse originalResponse = exchange.getResponse();
+      // 缓存数据的工厂
+      DataBufferFactory bufferFactory = originalResponse.bufferFactory();
+      // 拿到响应码
+      HttpStatus statusCode = originalResponse.getStatusCode();
+      if (statusCode == HttpStatus.OK) {
+        // 装饰，增强能力
+        ServerHttpResponseDecorator decoratedResponse =
+                new ServerHttpResponseDecorator(originalResponse) {
+                  // 等调用完转发的接口后才会执行
+                  @Override
+                  public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+                    log.info("body instanceof Flux: {}", (body instanceof Flux));
+                    if (body instanceof Flux) {
+                      Flux<? extends DataBuffer> fluxBody = Flux.from(body);
+                      // 往返回值里写数据
+                      // 拼接字符串
+                      return super.writeWith(
+                              fluxBody.map(
+                                      dataBuffer -> {
                                         // 7. 调用成功，接口调用次数 + 1 invokeCount
                                         try {
-                                            innerUserInterfaceInfoService.invokeCount(interfaceInfoId, userId);
+                                          innerUserInterfaceInfoService.invokeCount(interfaceInfoId, userId);
                                         } catch (Exception e) {
-                                            log.error("invokeCount error", e);
+                                          log.error("invokeCount error", e);
                                         }
                                         byte[] content = new byte[dataBuffer.readableByteCount()];
                                         dataBuffer.read(content);
-                                        DataBufferUtils.release(dataBuffer);//释放掉内存
+                                        DataBufferUtils.release(dataBuffer); // 释放掉内存
                                         // 构建日志
                                         StringBuilder sb2 = new StringBuilder(200);
                                         List<Object> rspArgs = new ArrayList<>();
                                         rspArgs.add(originalResponse.getStatusCode());
-                                        String data = new String(content, StandardCharsets.UTF_8); //data
+                                        String data = new String(content, StandardCharsets.UTF_8); // data
                                         sb2.append(data);
                                         // 打印日志
                                         log.info("响应结果：" + data);
                                         return bufferFactory.wrap(content);
-                                    }));
-                        } else {
-                            // 8. 调用失败，返回一个规范的错误码
-                            log.error("<--- {} 响应code异常", getStatusCode());
-                        }
-                        return super.writeWith(body);
+                                      }));
+                    } else {
+                      // 8. 调用失败，返回一个规范的错误码
+                      log.error("<--- {} 响应code异常", getStatusCode());
                     }
+                    return super.writeWith(body);
+                  }
                 };
-                // 设置 response 对象为装饰过的
-                return chain.filter(exchange.mutate().response(decoratedResponse).build());
-            }
-            return chain.filter(exchange); // 降级处理返回数据
-        } catch (Exception e) {
-            log.error("网关处理响应异常" + e);
-            return chain.filter(exchange);
-        }
+        // 设置 response 对象为装饰过的
+        return chain.filter(exchange.mutate().response(decoratedResponse).build());
+      }
+      return chain.filter(exchange); // 降级处理返回数据
+    } catch (Exception e) {
+      log.error("网关处理响应异常" + e);
+      return chain.filter(exchange);
     }
+  }
 
-    @Override
-    public int getOrder() {
-        return -1;
-    }
+  @Override
+  public int getOrder() {
+    return -1;
+  }
 
-    public Mono<Void> handleNoAuth(ServerHttpResponse response) {
-        response.setStatusCode(HttpStatus.FORBIDDEN);
-        return response.setComplete();
-    }
+  public Mono<Void> handleNoAuth(ServerHttpResponse response) {
+    response.setStatusCode(HttpStatus.FORBIDDEN);
+    return response.setComplete();
+  }
 
-    public Mono<Void> handleManyRequest(ServerHttpResponse response) {
-        response.setStatusCode(HttpStatus.TOO_MANY_REQUESTS);
-        return response.setComplete();
-    }
+  public Mono<Void> handleManyRequest(ServerHttpResponse response) {
+    response.setStatusCode(HttpStatus.TOO_MANY_REQUESTS);
+    return response.setComplete();
+  }
 
-    public Mono<Void> handleInvokeError(ServerHttpResponse response) {
-        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
-        return response.setComplete();
-    }
+  public Mono<Void> handleInvokeError(ServerHttpResponse response) {
+    response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+    return response.setComplete();
+  }
 }
